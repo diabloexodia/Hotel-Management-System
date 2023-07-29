2ae95407de4bd9a25d3cac1b3023b3effbb40e06
diabloexodia <diabloexodia@gmail.com>
2023-07-29 17:56:11 +0530

    first commit

---
+++ admin/db.php
@@ -0,0 +1,3 @@
+<?php
+$con = mysqli_connect("hotel.mysql.database.azure.com","admin1","Password1234","hotel",3306) or die(mysql_error());
+?>
---
+++ db.php
@@ -0,0 +1,5 @@
+<?php
+$con = mysqli_connect("hotel.mysql.database.azure.com","admin1","Password1234","hotel",3306) ;
+//or die(mysql_error());
+
+?>
