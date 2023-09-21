5a6f8ae0de5b237dc92b98bb1c16e9051c874a38
Dhyan Prasad <85845393+diabloexodia@users.noreply.github.com>
2023-09-21 12:37:22 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("hotel.mysql.database.azure.com","admin1","Password1234","hotel",3306) ;
+$con = mysqli_connect("hoteldb.csze4pgxgikq.ap-southeast-1.rds.amazonaws.com","admin","admin123","hotel",3306) ;
 //or die(mysql_error());
 
 ?>
